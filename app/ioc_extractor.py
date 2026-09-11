199f89a92343036ceb414f4532e86af88f23a621
cyber5957 <https://github.com/cyber5957>
2026-09-11 21:51:16 +0530

    basic ioc extration code done , moving to fixes and adancement

--- app/ioc_extractor.py
+++ app/ioc_extractor.py
@@ -1,8 +1,17 @@
 import re 
 
+def extract_iocs(text):
+    m = re.findall(r'[\w,-]+://[\w.,-]+',text)
+    print("URL: ",m[0])
+
+    n = re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', text)
+    print("ip address: ",n[0])
+
+    o = re.findall(r'[a-fA-F0-9]{32}',text)
+    print( "hash value: ", o[0])
+
 text = """User clicked http://secure-login-update.com from IP 185.220.101.45.
 File hash observed: d41d8cd98f00b204e9800998ecf8427e"""
 
-m = re.findall(r'[\w,-]+://[\w,-]+',text)
-print("URL: ",m)
+extract_iocs(text)
 
