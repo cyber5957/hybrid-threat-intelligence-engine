e5918e71f98243613722779e7ddb071b3bb50ba5
cyber5957 <https://github.com/cyber5957>
2026-09-11 21:04:24 +0530

    commit start

--- app/ioc_extractor.py
+++ app/ioc_extractor.py
@@ -1,1 +1,8 @@
-print("hello world")
+import re 
+
+text = """User clicked http://secure-login-update.com from IP 185.220.101.45.
+File hash observed: d41d8cd98f00b204e9800998ecf8427e"""
+
+m = re.findall(r'[\w,-]+://[\w,-]+',text)
+print("URL: ",m)
+
