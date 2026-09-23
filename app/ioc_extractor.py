05e35db2f599d0169ad77ff5e2d2d75a6d6f29c3
cyber5957 <https://github.com/cyber5957>
2026-09-23 10:17:00 +0530

    duplicates removal logic

--- app/ioc_extractor.py
+++ app/ioc_extractor.py
@@ -1,9 +1,16 @@
 import re
 
-alert = "The endpoint is connected to 185.220.1.3"
-
+alert = """
+Connection from 185.220.101.5 detected.
+Connection from 192.168.1.20 detected.
+Connection from 185.220.101.5 detected again.
+Connection from 10.10.10.45 detected.
+Connection from 192.168.1.20 detected again.
+"""
 pattern = r"\b\d{1,3}(?:\.\d{1,3}){3}\b"
 
 ip_address = re.findall(pattern, alert)
 
-print(ip_address)
+remove_duplicates = list(dict.fromkeys(ip_address))
+
+print(remove_duplicates)
